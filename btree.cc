f26e1cc000100bfc8cbe7e9ebf10ad9daeffec7a
xiaolongnk <xiaolongnk@126.com>
2014-07-17 23:01:30 +0800

    this is my first version.

--- btree.cc
+++ btree.cc
@@ -4,11 +4,37 @@
 using namespace std;
 
 
+#define N 3
 
+typedef int elem;
+
+struct node {
+    elem data[N];
+    node *child[N+1];
+    int cnt;
+    
+    node(){
+        for(int i=0; i<N; i++){
+            data[i] = 0;
+            child[i] = NULL;
+        }
+        child[N] = NULL;
+        cnt = 0;
+    }
+};
+
+// insert key in the tree;
+
+// delete key in tree;
+
+// print tree
+
+// search key in tree
+
+//          
 
 int main(){
-
-
+    
     return 0;
 }
 
