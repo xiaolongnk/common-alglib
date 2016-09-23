6d487e9b4b78011a39eab64e43ac0a7808892c21
xiaolongnk <xiaolongnk@126.com>
2016-09-23 19:01:46 +0800

    solve ugly number

---
+++ leetcode/ag_263.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isUgly(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        if num == 0:
+            return False
+        for x in [2,3,5]:
+            while num % x == 0:
+                num = num / x
+        return num == 1?False:True
+
+
+def run():
+    so = Solution()
+    print so.isUgly(932)
