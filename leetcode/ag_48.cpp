473eba643c08e4ba3ad86f66ee794d1ef5dedf09
xiaolongnk <xiaolongnk@126.com>
2016-11-07 13:49:54 +0800

    add tpl , solve image rotate problem

---
+++ leetcode/ag_48.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <list>
+#include <queue>
+#include <stack>
+#include <algorithm>
+
+
+using namespace std;
+
+
+class Solution {
+private:
+    int length;
+    int width;
+public:
+    Solution(int a , int b):length(a),width(b){};
+
+    void rotate(vector<vector<int>>& matrix) {
+        reverse(matrix.begin() , matrix.end());
+        for(auto i = 0; i < matrix.size()-1; ++i) {
+            for(auto j = i + 1 ; j < matrix[i].size(); ++j){
+                swap(matrix[i][j] , matrix[j][i]);
+            }
+        }
+    }
+
+    void show()
+    {
+        cout<<"length "<< length <<"width "<<width<<endl;
+    }
+};
+
+void printMatirx(vector<vector<int>> s)
+{
+    for(auto i = 0 ; i < s.size() ; ++i){
+        for(auto x : s[i]){
+            cout<<x<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+
+int main()
+{
+    Solution a(1,2);
+    vector<int> s1 = {1,2,3};
+    vector<int> s2 = {4,5,6};
+    vector<int> s3 = {7,8,9};
+    vector<vector<int>> ww = {s1 , s2 , s3};
+    printMatirx(ww);
+    a.rotate(ww);
+    printMatirx(ww);
+
+    a.show();
+    return 0;
+}
---
+++ leetcode/cpph.tpl
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <list>
+#include <queue>
+#include <stack>
+#include <algorithm>
+
+
+using namespace std;
+
+
