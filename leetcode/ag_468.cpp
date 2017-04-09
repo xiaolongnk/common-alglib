955cb978ddcbfa71bbf0c7e4aad0d8122ddb9467
xiaolongnk <xiaolongnk@126.com>
2017-04-09 16:21:33 +0800

    add code

--- leetcode/ag_468.cpp
+++ leetcode/ag_468.cpp
@@ -13,27 +13,70 @@ using namespace std;
 
 
 class Solution {
-public:
-    string validIPAddress(string IP) {
-               
-    }
+    public:
+        string validIPAddress(string IP) {
+            const string ans[3] = {"IPv4" , "IPv6" , "Neither"};
+            const char comma = ':';
+            const char dot = '.';
+            stringstream nss(IP);
+            string block;
+            if(IP.substr(0 , 4).find(dot) != string::npos) {
+                int i = 0;
+                while(getline(nss , block , dot)) {
+                    ++i;
+                    if(!ip4bv(block)) return ans[2];
+                    block.clear();
+                }
+                cout<<block<<endl;
+                cout<<i<<endl;
+                if( i > 4) return ans[2];
+                return ans[0];
+            } else if (IP.substr(0 , 5).find(comma) != string::npos) {
+                int i = 0;
+                while(getline(nss , block, dot)){
+                    ++i;
+                    if(!ip6bv(block)) return ans[2];
+                    block.clear();
+                }
+                cout<<block<<endl;
+                if( i > 6 ) return ans[2];
+                return ans[1];
+            }
+            return ans[2];
+        }
+
+        bool ip4bv(string &block) {
+            if(block.length() > 3) return false;
+            short sum = 0;
+            if(block[0] == '0' && block.length() > 1) return false;
+            for(int i =0 ; i < block.length() ; ++i) {
+                if( block[i] < '0' && block[i] >'9' ) return false;
+                sum += (block[i] - '0') * 10; 
+            }
+            if(sum>255) return false;
+            return true;
+        }
+
+        bool ip6bv(string &block) {
+            if(block.length() >4 ) return false;
+            const string ip6charset ="0123456789abcdefABCDEF";
+            if(block[0] == '0' && block.length() > 1) return false;
+            for(int i = 0 ; i < block.length() ; ++i) {
+                if(ip6charset.find(block[i])==string::npos) {
+                    return false;
+                }
+            }
+            return true;
+        }
 };
 
 
 int main()
 {
-    string str = "172.30.21.8";
-    string ip6_str = "a2:b1:b2:30:20:bb";
-    if(str.substr(0 , 4).find('.') != string::npos) {
-        cout<<"ipv4"<<endl;
-    }else {
-        cout<<"ipv6"<<endl;
-    }
-    stringstream nss(str);
-    string block;
-    for(int i = 0 ; i < 4; ++i) {
-        getline(nss , block, '.');
-        cout<<block<<endl;
+    string str = "";
+    Solution s;
+    while(cin>>str) {
+        cout<<str<<"   :   "<<s.validIPAddress(str)<<endl;
     }
     return 0;
 }
