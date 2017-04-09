8114fb265e59b8ccd54439edbccaae991f628a70
xiaolongnk <xiaolongnk@126.com>
2017-04-09 23:22:07 +0800

    solve ag_468

--- leetcode/ag_468.cpp
+++ leetcode/ag_468.cpp
@@ -18,28 +18,45 @@ class Solution {
             const string ans[3] = {"IPv4" , "IPv6" , "Neither"};
             const char comma = ':';
             const char dot = '.';
-            stringstream nss(IP);
             string block;
             if(IP.substr(0 , 4).find(dot) != string::npos) {
-                int i = 0;
-                while(getline(nss , block , dot)) {
-                    ++i;
-                    if(!ip4bv(block)) return ans[2];
-                    block.clear();
+                int dotcnt = 0;
+                for(int j = 0 ; j < IP.length(); ++j) {
+                    if(IP[j] == dot || (IP[j]!=dot && j == IP.size() - 1)) {
+                        if(IP[j] == dot) dotcnt++;
+                        if(!ip4bv(block)) return ans[2];
+                        block.clear();
+                    } else {
+                        block += IP[j];
+                    }
                 }
-                cout<<block<<endl;
-                cout<<i<<endl;
-                if( i > 4) return ans[2];
+                if(dotcnt>3) return ans[2];
                 return ans[0];
             } else if (IP.substr(0 , 5).find(comma) != string::npos) {
-                int i = 0;
-                while(getline(nss , block, dot)){
-                    ++i;
-                    if(!ip6bv(block)) return ans[2];
-                    block.clear();
+                int commacnt = 0;
+                int conjective_flag = 0;
+                for(int j = 0; j < IP.size(); ++j) {
+                    if(IP[j] == comma || (IP[j]!=comma && j == IP.size() - 1)) {
+                        if(IP[j] == comma) commacnt++;
+                        if(!ip6bv(block)) return ans[2];
+                        block.clear();
+                    } else {
+                        block += IP[j];
+                    }
+                    if(j && IP[j] == IP[j-1] && IP[j] == comma ) {
+                        // no 2 conjective comma
+                        if(conjective_flag) return ans[2];
+                        else conjective_flag = 1;
+                    }
                 }
-                cout<<block<<endl;
-                if( i > 6 ) return ans[2];
+                if(!conjective_flag) {
+                    // comma not enough
+                    if(commacnt < 7) return ans[2];
+                } else {
+                    if(commacnt > 5) return ans[2];
+                }
+                // too many comma
+                if(commacnt>7) return ans[2];
                 return ans[1];
             }
             return ans[2];
@@ -60,7 +77,6 @@ class Solution {
         bool ip6bv(string &block) {
             if(block.length() >4 ) return false;
             const string ip6charset ="0123456789abcdefABCDEF";
-            if(block[0] == '0' && block.length() > 1) return false;
             for(int i = 0 ; i < block.length() ; ++i) {
                 if(ip6charset.find(block[i])==string::npos) {
                     return false;
