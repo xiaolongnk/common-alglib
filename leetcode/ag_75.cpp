cc29808e0b15e3fcd30c4e8f57041859d6299e90
xiaolongnk <xiaolongnk@126.com>
2017-04-11 19:50:22 +0800

    solve sort colors

--- leetcode/ag_75.cpp
+++ leetcode/ag_75.cpp
@@ -9,32 +9,55 @@
 
 using namespace std;
 
+void printVector(vector<int>& test) {
+    for(const auto & x : test) {
+        cout<<x<<" ";
+    }
+    cout<<endl;
+}
 
 class Solution {
-	public:
-		void sortColors(vector<int>& nums) {
+    public:
+        void sortColors(vector<int>& nums) {
             int start_0 = 0 , start_2 = nums.size() - 1;
-            for(int i = 0 ; i < nums.size() ; i++) {
-                if(nums[i] == 0) swap(nums[start_0++] , nums[i]);
-                if(nums[i] == 2) swap(nums[start_2--] , nums[i]);
+            for(int i = 0 ; i <= start_2 ; i++) {
+                int tmp_val = nums[i];
+                if(tmp_val == 0) swap(nums[start_0++] , nums[i]);
+                else if(tmp_val == 2) {
+                    while(nums[start_2] == 2) {
+                        --start_2;
+                    }
+                    if(i > start_2) break;
+                    swap(nums[i] , nums[start_2]);
+                    if(nums[i] == 0) --i;
+                    --start_2;
+                }
             }
-            for(const auto & x : nums) {
-                cout<<x<<" ";
-            }
-            cout<<endl;
-		}
+        }
 };
-
+void printLine()
+{
+    cout<<"--------------"<<endl;
+}
 int main()
 {
     Solution s;
-    int arr[] = {0,2,0,0,1,1,1,2,0,0};
-    vector<int> ans ;
-    for(int i = 0 ; i < 10; ++i) {
-        ans.push_back(arr[i]);
-        cout<<arr[i]<<" ";
+    vector<int> ans;
+    int total , tmp;
+    cin>>total;
+    int case_num = 0;
+    for(int i = 0 ; i < total ; ++i) {
+        cin>>case_num;
+        for(int _i = 0; _i < case_num ; ++_i) {
+            cin>>tmp;
+            ans.push_back(tmp);
+        }
+        printLine();
+        printVector(ans);
+        s.sortColors(ans);
+        printVector(ans);
+        printLine();
+        ans.clear();
     }
-    cout<<endl;
-    s.sortColors(ans);
-	return 0;
+    return 0;
 }
