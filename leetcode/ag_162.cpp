fb4baf7d443afe394486d1ee1bc54e8056404677
xiaolongnk <xiaolongnk@126.com>
2017-04-12 17:31:03 +0800

    solve problem ag_162 , find peak element in array.

--- leetcode/ag_162.cpp
+++ leetcode/ag_162.cpp
@@ -8,31 +8,63 @@
 #include <string>
 
 using namespace std;
+
+void printVector(vector<int>& test) {
+    for(const auto & x : test) {
+        cout<<x<<" ";
+    }
+    cout<<endl;
+}
+
+
 class Solution {
-	public:
-		int findPeakElement(vector<int>& nums) {
-            int l = 0 , r = nums.size() - 1;
+    public:
+        int findPeakElement(vector<int>& nums) {
+            int l = 0 , r = nums.size() - 1 , m = 0;
             while(l <= r ) {
-                int m = (l + r)>>1;
-                if(m == 0 && nums[m] > nums[m+1]) {
-                    return m;
-                }else if(m == nums.size() - 1 && nums[m] > nums[m-1]) {
-                    return m;
-                }else if(nums[m] > nums[m-1] && nums[m] > nums[m+1]) {
-                    return m;
-                }else if(nums[m] > nums[m-1] && nums[m] < nums[m+1]) {
-                    l = m;    
-                }else if(nums[m] < nums[m-1] && nums[m] > nums[m+1] ) {
-                    r = m;
+                m = (l + r)>>1;
+                if( (m == 0 && m+1<=nums.size() 
+                            && nums[m] > nums[m+1]) ||
+                    (m == nums.size() - 1 && nums[m] > nums[m-1]) ||
+                    (nums[m] > nums[m-1] && nums[m] > nums[m+1]) )
+                {
+                    break;
+                }else if((m==0 && m+1 < nums.size() && 
+                            nums[m] < nums[m+1])||
+                        (nums[m]>nums[m-1] && nums[m]<nums[m+1])) 
+                {
+                    l == m ? ++l :l = m;
+                }else if((nums[m]<nums[m-1] && nums[m]>nums[m+1]) ||
+                        (nums[m]<nums[m-1] && nums[m]<nums[m+1]))
+                {
+                    r == m ? --r : r = m;
                 }
             }
-            // not found.
-            return -1;
-		}
+            return m;
+        }
 };
 
+void get_input()
+{
+    int total_line = 0;
+    int total = 0;
+    vector<int> ans;
+    cin>>total_line;
+    for(int i = 0 ; i < total_line ; ++i) {
+        cin>>total;
+        for(int i =0; i < total; ++i) {
+            int tmp;
+            cin>>tmp;
+            ans.push_back(tmp);
+        }
+        Solution s ;
+        cout<<s.findPeakElement(ans)<<endl;
+        ans.clear();
+    }
+}
 
 int main()
 {
-	return 0;
+    get_input();
+    return 0;
 }
