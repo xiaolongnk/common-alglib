6e1934d15b797d46159e4868bf619be9f9c04442
xiaolongnk <xiaolongnk@126.com>
2017-05-08 13:18:02 +0800

    try to finish class 9 source file.

---
+++ class/level1/class_9.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define MAX 100
+
+int main()
+{
+    int total = 0;
+    int start_pos = 0;
+    int flag_arr[MAX];
+    for(int i = 0 ; i < MAX ; ++i) flag_arr = [1];
+
+    while(1) {
+        printf("please input total people(<%d):",MAX);
+        scanf("%d", &total);
+        printf("please input start pos(<%d) :",total);
+        scanf("%d", &start_pos);
+    }
+
+    return 0;
+}
