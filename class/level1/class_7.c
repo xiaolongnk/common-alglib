8b9001f11fa86e3a9aee082c17287b0029c1d7d9
xiaolongnk <xiaolongnk@126.com>
2017-05-08 11:15:10 +0800

    add define in program

--- class/level1/class_7.c
+++ class/level1/class_7.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
+#define PRIME_START 2
 void printPrimer(int num)
 {
     const int number_width = 10;
     int cnt = 0;
-    for(int i = 2 ; i < num ; ++i) {
+    for(int i = PRIME_START ; i < num ; ++i) {
         int is_p = 1;
-        for(int j = 2 ; j < i ; ++j) {
+        for(int j = PRIME_START ; j < i ; ++j) {
             if((i % j) == 0) {
                 is_p = 0;
                 break;
