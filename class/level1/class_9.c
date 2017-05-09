a9cd9cc8ff8c43160f8e98b7059b8e6ce2e1ecc1
xiaolongnk <xiaolongnk@126.com>
2017-05-10 00:24:24 +0800

    solve class 9 problem

--- class/level1/class_9.c
+++ class/level1/class_9.c
@@ -4,12 +4,38 @@
 
 typedef const int cint;
 
+void printCircle(int *p , cint total)
+{
+    for(int i = 0 ; i < total ; ++i) {
+        if(p[i] == 1) printf("%d  " , i+1);
+        else printf("   ");
+    }
+    printf("\n");
+}
 
 void printResult(int *p , cint start_pos , cint total , cint interval)
 {
-    int cnt = 0;
-    while(cnt < total - 1) {
-        for()
+    if(start_pos < 1) {
+        printf("start pos must > 0 \n");
+        return;
+    }
+    printCircle(p , total);
+
+    int dead_cnt = 0;
+    int cursor = start_pos;
+    int _interval = 0;
+    while(dead_cnt < total - 1) {
+        if(p[cursor - 1] == 1) {
+            _interval++;
+        }
+        if(_interval == interval) {
+            p[cursor - 1] = 0;
+            ++dead_cnt;
+            printCircle(p , total);
+            _interval = 0;
+        }
+        if(cursor == total ) cursor = 1;
+        else ++cursor;
     }
 }
 
@@ -25,9 +51,13 @@ L0:
         printf("please input total people(<%d):",MAX);
         scanf("%d", &total);
         if(total > MAX) {
-            printf("number of total must less than %d , please input again\n");
+            printf("number of total must less than %d , please input again\n" , MAX);
             goto L0;
         }
+        if(total == -1) {
+            printf("Bye Bye!\n");
+        }
+        return 0;
 L1: 
         printf("please input start pos(<%d) :",total);
         scanf("%d", &start_pos);
@@ -35,11 +65,10 @@ L1:
             printf("number of start pos must less than %d , please input again\n" , total);
             goto L1;
         }
-        printf("please input your interval\n");
+        printf("please input your interval:");
         scanf("%d" , &interval);
-        for(int i = 0 ; i < total; ++i) flag_arr = [1];
+        for(int i = 0 ; i < total; ++i) flag_arr[i] = 1;
         printResult(flag_arr , start_pos , total , interval);
-
     }
 
     return 0;
