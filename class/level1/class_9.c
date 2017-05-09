ebe840560378e55fa4986dc6e4bfd8ee8ec6184c
xiaolongnk <xiaolongnk@126.com>
2017-05-09 09:23:15 +0800

    some code for class9

--- class/level1/class_9.c
+++ class/level1/class_9.c
@@ -2,18 +2,44 @@
 
 #define MAX 100
 
+typedef const int cint;
+
+
+void printResult(int *p , cint start_pos , cint total , cint interval)
+{
+    int cnt = 0;
+    while(cnt < total - 1) {
+        for()
+    }
+}
+
 int main()
 {
     int total = 0;
     int start_pos = 0;
+    int interval = 0;
     int flag_arr[MAX];
-    for(int i = 0 ; i < MAX ; ++i) flag_arr = [1];
 
     while(1) {
+L0:
         printf("please input total people(<%d):",MAX);
         scanf("%d", &total);
+        if(total > MAX) {
+            printf("number of total must less than %d , please input again\n");
+            goto L0;
+        }
+L1: 
         printf("please input start pos(<%d) :",total);
         scanf("%d", &start_pos);
+        if( start_pos > total) {
+            printf("number of start pos must less than %d , please input again\n" , total);
+            goto L1;
+        }
+        printf("please input your interval\n");
+        scanf("%d" , &interval);
+        for(int i = 0 ; i < total; ++i) flag_arr = [1];
+        printResult(flag_arr , start_pos , total , interval);
+
     }
 
     return 0;
