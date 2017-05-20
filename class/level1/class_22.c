06856a41d7207977158f2f2a2e78117ba77e2f5b
xiaolongnk <xiaolongnk@126.com>
2017-05-20 23:46:51 +0800

    solve eight queen problems

--- class/level1/class_22.c
+++ class/level1/class_22.c
@@ -9,10 +9,48 @@
 #include <string.h>
 #include <malloc.h>
 
+#define N 15
+
+int total_solution = 1;
+int pos[N];
+
+int solve_n_queen_problems(int total , int current_line)
+{
+    if(current_line == total) {
+        printf("Solution %d\n",total_solution++);
+        for(int i = 0 ; i < total ; ++i) {
+            for(int j = 0 ; j < total ; ++j) {
+                if(j == pos[i]) printf(" Q ");
+                else printf(" * ");
+            }
+            printf("\n");
+        }
+    } else {
+        for(int col = 0 ; col < total ; ++col ) {
+            //check if the [step,i] can be ok to place element.
+            int flag = 1;
+            for(int line = 0 ; line < current_line; ++line) {
+                if(pos[line] == col ||  col - pos[line]  == current_line - line  || 
+                        (current_line - line  == pos[line] - col)) {
+                    flag = 0;
+                    break;
+                }
+            }
+            if(flag == 1) {
+                pos[current_line] = col;
+                solve_n_queen_problems(total , current_line+ 1);
+                pos[current_line] = -1;
+            }
+        }
+    }
+}
 
 int main()
 {
-
+    for(int i = 0 ; i < N ; ++i) {
+        pos[i] = -1;
+    }
+    solve_n_queen_problems(6,0);
     return 0;
 }
 
