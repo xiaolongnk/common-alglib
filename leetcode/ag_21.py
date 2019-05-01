62b895cd8909443ead89cf033760d14e5d258847
ouxiaolong <ouxiaolong@qudian.com>
2019-05-01 13:56:40 +0800

    finish leetcode 21, easy problem , merge two sorted linked list

---
+++ leetcode/ag_21.py
@@ -0,0 +1,41 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        if l1 == None:
+            return l2
+        if l2 == None:
+            return l1
+        if l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
+
+    def printList(self, head):
+        while head:
+            print head.val,
+            head = head.next
+
+def run():
+    l1 = ListNode(1)
+    l2 = ListNode(3)
+    r1 = ListNode(2)
+    r2 = ListNode(4)
+    l1.next = l2
+    r1.next = r2
+
+    s = Solution()
+    res = s.mergeTwoLists(l1, r1)
+    s.printList(res)
