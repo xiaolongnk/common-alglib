d6a113b88512d86f3632d8a67334e28ab42869e3
xiaolongnk <xiaolongnk@126.com>
2019-06-06 08:52:41 +0800

    finish ag_29

--- leetcode/ag_29.py
+++ leetcode/ag_29.py
@@ -8,12 +8,33 @@ class Solution(object):
         sign = -1
         if (dividend < 0 and divisor < 0) or (dividend > 0 and divisor > 0):
             sign = 1
-        if divisor == 1:
-            return dividend if sign == 1 else -dividend
-        i = 0
+        if divisor == -1 and dividend == -2147483648:
+            return 2147483647
+        divisor = abs(divisor)
+        dividend = abs(dividend)
+        if dividend < divisor:
+            return 0
+        origin_divisor = divisor
+        q = 1
+        rq = 0
         while True:
-            a = divisor << i
-            b = divisor << (i + 1)
+            t = divisor << 1
+            if dividend >= t:
+                divisor = t
+                q += q
+            else:
+                rq += q
+                q = 1
+                dividend -= divisor
+                divisor = origin_divisor
+                if dividend < (divisor << 1):
+                    if dividend >= divisor:
+                        rq += 1
+                    break
+        return -rq if sign == -1 else rq
 
 def run():
-    pass
+    s = Solution()
+    ans = s.divide(331, 1)
+    print ans
+
