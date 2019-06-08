e0ecef75ff11bf1ae02fa1a4ec513669f24b2687
xiaolongnk <xiaolongnk@126.com>
2019-06-08 09:54:09 +0800

    add a better solution for ag_03

--- leetcode/ag_03.py
+++ leetcode/ag_03.py
@@ -44,6 +44,22 @@ class Solution(object):
             hashmap[ord(s[i])] = i
         return maxLen
 
+    def lengthOfLongestSubstringV2(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        used = {}
+        start = 0
+        mx = 0
+        for i,c in enumerate(s):
+            if c in used and start <= used[c]:
+                start = used[c] + 1
+            else:
+                mx = max(mx, i - start + 1)
+            used[c] = i
+        return mx
+
 def run():
     s = Solution()
     test = "casdkfalsdfjuiy"
